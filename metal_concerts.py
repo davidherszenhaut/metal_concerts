202b50987b35a362a6a8632eee13719a0c15c9b1
David Herszenhaut <davidherszenhaut@users.noreply.github.com>
2017-04-21 14:20:33 -0400

    Added genre finding to metal_concerts.py

--- metal_concerts.py
+++ metal_concerts.py
@@ -15,10 +15,8 @@ __email__ =      "david.herszenhaut@gmail.com"
 __status__ =     "Development"
 
 # TODO
-# print band names without underscores (seperate lists)
-# check genres
 # try to make some kind of index.html
-# add to dictionary
+# use python -m http.server and webbrowser to open index.html
 
 # get .html as text
 request = urllib.request.Request("http://www.wrekage.org/events.php")
@@ -29,22 +27,22 @@ page_text = page_bytes.decode()
 # get list of bands because they are all in <strong> tags
 band_list = re.findall(r'<strong>(.+?)</strong>', page_text.replace("\n", ""))
 
-# remove the last 15 bands because they are not really there
+# remove the last 18 bands because they are not really there
 band_list = band_list[:-18]
 
 # replace spaces with underscores for url purposes
-band_list = [band.replace(" ", "_").strip() for band in band_list]
+band_list_underscore = [band.replace(" ", "_").strip() for band in band_list]
 
 # make an empty dictionary for band (str): genre (str)
 band_dict = {}
 
 # loop over band list to fill dictionary
-for current_band in band_list:
+for i in range(len(band_list)):
 
     try:
 
         # get .html as text
-        current_request = urllib.request.Request("http://www.metal-archives.com/bands/" + current_band)
+        current_request = urllib.request.Request("http://www.metal-archives.com/bands/" + band_list_underscore[i])
         current_response = urllib.request.urlopen(current_request)
         current_bytes = current_response.read()
         current_text = current_bytes.decode()
@@ -54,10 +52,24 @@ for current_band in band_list:
         current_genre = dd_tags[3]
 
         # add to dictionary
-        band_dict[current_band] = current_genre
+        band_dict[band_list[i]] = current_genre
     
-        print("This band is kvlt (" + current_band + ").")
+        # print("This band is kvlt (" + current_band + ").")
+        print("The band " + band_list[i] + " was found. Its genre is " + band_dict[band_list[i]] + ".")
 
     except:
 
-        print("This band is not kvlt (" + current_band + ").")
+        # print("This band is not kvlt (" + current_band + ").")
+        print("The band " + band_list[i] + " was not found.")
+
+for band, genre in band_dict.items():
+    if "doom" in genre.lower():
+        print(band + " plays doom metal (" + genre + ").")
+    elif "melodic" in genre.lower():
+        print(band + " plays melodic metal (" + genre + ").")
+    elif "black" in genre.lower():
+        print(band + " plays black metal (" + genre + ").")
+    elif "heavy" in genre.lower():
+        print(band + " plays heavy metal (" + genre + ").")
+    elif "power" in genre.lower():
+        print(band + " plays power metal (" + genre + ").")
