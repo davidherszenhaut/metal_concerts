11d80f317b55d31d1321a7ea28a553e695b2377e
David Herszenhaut <davidherszenhaut@users.noreply.github.com>
2017-06-12 18:37:34 -0400

    Updated script
     - Rewrote comments to make more sense
     - Added if __name__ == "__main__": section
     - Allowed user to edit preferred style
     - Fixed issue with bands separated by commas not separating

--- metal_concerts.py
+++ metal_concerts.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
-"""Script to look at metal concerts in Atlanta and produce a static website representing the ones worth attending."""
+"""Script to look up metal concerts in Atlanta"""
+
+
 
 import urllib.request
 import re
+
+
 
 __author__ =     "David Herszenhaut"
 __copyright__ =  "Copyright 2017"
@@ -14,62 +18,88 @@ __maintainer__ = "David Herszenhaut"
 __email__ =      "david.herszenhaut@gmail.com"
 __status__ =     "Development"
 
+
+
 # TODO
-# try to make some kind of index.html
-# use python -m http.server and webbrowser to open index.html
+# create HTML and local webserver
 
-# get .html as text
-request = urllib.request.Request("http://www.wrekage.org/events.php")
-response = urllib.request.urlopen(request)
-page_bytes = response.read()
-page_text = page_bytes.decode()
 
-# get list of bands because they are all in <strong> tags
-band_list = re.findall(r'<strong>(.+?)</strong>', page_text.replace("\n", ""))
 
-# remove the last 18 bands because they are not really there
-band_list = band_list[:-18]
+def get_band_list():
 
-# replace spaces with underscores for url purposes
-band_list_underscore = [band.replace(" ", "_").strip() for band in band_list]
+    # get calendar HTML as text
+    request = urllib.request.Request("http://www.wrekage.org/events.php")
+    response = urllib.request.urlopen(request)
+    page_bytes = response.read()
+    page_text = page_bytes.decode()
 
-# make an empty dictionary for band (str): genre (str)
-band_dict = {}
+    # get list of performing bands in <strong> tags
+    band_list = re.findall(r'<strong>(.+?)</strong>', page_text.replace("\n", ""))
 
-# loop over band list to fill dictionary
-for i in range(len(band_list)):
+    # remove last 18 bands because they are not part of the calendar
+    band_list = band_list[:-18]
 
-    try:
+    # split any bands separated by commas
+    band_list = [band.split(",") for band in band_list]
 
-        # get .html as text
-        current_request = urllib.request.Request("http://www.metal-archives.com/bands/" + band_list_underscore[i])
-        current_response = urllib.request.urlopen(current_request)
-        current_bytes = current_response.read()
-        current_text = current_bytes.decode()
+    # flatten any nested lists created
+    band_list = [x for y in band_list for x in y]
 
-        # find genre inside <dd> tags
-        dd_tags = re.findall(r'<dd>(.+?)</dd>', current_text.replace("\n", ""))
-        current_genre = dd_tags[3]
+    band_list = [band.strip() for band in band_list]
 
-        # add to dictionary
-        band_dict[band_list[i]] = current_genre
-    
-        # print("This band is kvlt (" + current_band + ").")
-        print("The band " + band_list[i] + " was found. Its genre is " + band_dict[band_list[i]] + ".")
+    return band_list
 
-    except:
+def get_band_genres(band_list):
 
-        # print("This band is not kvlt (" + current_band + ").")
-        print("The band " + band_list[i] + " was not found.")
+    # create list with underscores for url creation
+    band_list_underscore = [band.replace(" ", "_").strip() for band in band_list]
 
-for band, genre in band_dict.items():
-    if "doom" in genre.lower():
-        print(band + " plays doom metal (" + genre + ").")
-    elif "melodic" in genre.lower():
-        print(band + " plays melodic metal (" + genre + ").")
-    elif "black" in genre.lower():
-        print(band + " plays black metal (" + genre + ").")
-    elif "heavy" in genre.lower():
-        print(band + " plays heavy metal (" + genre + ").")
-    elif "power" in genre.lower():
-        print(band + " plays power metal (" + genre + ").")
+    # make an empty dictionary for band(str): genre(str)
+    band_dict = {}
+
+    # populate dictionary with metal-archives.com information
+    for i in range(len(band_list)):
+
+        try:
+
+            # get band page HTML as text
+            current_request = urllib.request.Request("http://www.metal-archives.com/bands/" + band_list_underscore[i])
+            current_response = urllib.request.urlopen(current_request)
+            current_bytes = current_response.read()
+            current_text = current_bytes.decode()
+
+            # find genre in <dd> tags
+            dd_tags = re.findall(r'<dd>(.+?)</dd>', current_text.replace("\n", ""))
+            current_genre = dd_tags[3]
+
+            # add band and genre to band_dict
+            band_dict[band_list[i]] = current_genre
+
+            print("The band " + band_list[i] + " was found. Its genre is " + band_dict[band_list[i]] + ".")
+
+        except:
+
+            # band was not found
+            print("The band " + band_list[i] + " was not found.")
+
+    return band_dict
+
+
+
+def bands_with_style(band_dict, genre_list):
+
+    for band, genre in band_dict.items():
+        for style in genre_list:
+            if style in genre.lower():
+                print(band + " plays with the style: " + style + ".")
+
+if __name__ == "__main__":
+
+    print("Enter a space-separated list of the genres you like:")
+    genre_list = input("> ")
+    genre_list = genre_list.split(" ")
+    genre_list = [genre.lower() for genre in genre_list]
+
+    band_list = get_band_list()
+    band_dict = get_band_genres(band_list)
+    bands_with_style(band_dict, genre_list)
